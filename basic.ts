f4bf2e25397a3e8b7bafbd6167762c1ebb8cd75b
syedaureefaaly <161452433+syedaureefaaly@users.noreply.github.com>
2024-03-05 16:09:00 +0500

    Add files via upload
    My another code

---
+++ basic.ts
@@ -0,0 +1,18 @@
+//Question6.
+
+console.log(4 + 4);
+console.log(10 - 2); 
+console.log(2 * 4);
+console.log(16 / 2);
+
+//Question7.
+
+console.log(`${5 + 3}
+${10 - 2}
+${2 * 4}
+${16 / 2}`);
+
+//Question8.
+
+let favNum: number = 7;
+console.log(`My favorite number is ${favNum}`);
